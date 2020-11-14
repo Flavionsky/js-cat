8249c708c41351be0a3f290855bee1adf0a8ee47
Flavio Muscente <flaviomuscente@libero.it>
2020-11-15 00:52:15 +0100

    simplified code

--- js/main.js
+++ js/main.js
@@ -27,110 +27,85 @@ $(document).ready(function() {
     },
   ]
 
-  let catsEl = document.getElementById("cats");
-  let cont = 0;
   //ciclo l'array e inserisco sull'HTML
   cats.forEach((element) => {
 
     //creo la classe cat
     let cat = document.createElement("div");
     cat.classList.add("col-lg-12");
-    catsEl.appendChild(cat);
-    //creo la classe cat color per l'assegnamento del colore tramite numerazione
+    $("#cats").append(cat);
+    //creo la classe cat color per l'assegnamento del colore
     let catColor = document.createElement("span");
-    catColor.classList.add(cont);
-    cat.appendChild(catColor);
+    cat.append(catColor);
     catColor.innerHTML += '<i class="fas fa-2x fa-cat"></i>';
     catColor.style.color = element.color;
     //creo la classe per il nome
     let catName = document.createElement("span");
-    cat.appendChild(catName);
+    cat.append(catName);
     catName.innerHTML += element.name;
-    cont++;
   });
 
   //separo in due l'array in base al genere
 
-  const femaleCats = cats.filter((el) => {
-    if(el.gender == "female"){
+  const femaleCats = cats.filter((el) => el.gender == "female");
 
-      let newel = el;
-      return newel;
-
-    }
-  });
-  const maleCats = cats.filter((el) => {
-    if(el.gender == "male"){
-
-      let newel = el;
-      return newel;
-
-    }
-  });
-
-  let catsSeparetedEl = document.getElementById("catsSep");
+  const maleCats = cats.filter((el) => el.gender == "male");
 
 
   //creo la classe female
   let catFemale = document.createElement("div");
   catFemale.classList.add("col-6");
-  catsSeparetedEl.appendChild(catFemale);
+  $("#catsSep").append(catFemale);
 
   femaleCats.forEach((element) => {
 
     //creo la classe cat
     let cat = document.createElement("div");
-    catFemale.appendChild(cat);
-    //creo la classe cat color per l'assegnamento del colore tramite numerazione
+    catFemale.append(cat);
+    //creo la classe cat color per l'assegnamento del colore
     let catColor = document.createElement("span");
-    catColor.classList.add(cont);
-    cat.appendChild(catColor);
+    cat.append(catColor);
     catColor.innerHTML += '<i class="fas fa-2x fa-cat"></i>';
     catColor.style.color = element.color;
     //creo la classe per il nome
     let catName = document.createElement("span");
-    cat.appendChild(catName);
+    cat.append(catName);
     catName.innerHTML += element.name;
     //creo la classe per l'assegnazione del sesso
     let catGender = document.createElement("span");
-    cat.appendChild(catGender);
+    cat.append(catGender);
     catGender.innerHTML += '<i class="fas fa-2x fa-venus"></i>';
     catGender.style.color = "pink";
     catGender.style.opacity = element.age;
-    cont++;
   });
   //creo la classe male
   let catMale = document.createElement("div");
   catMale.classList.add("col-6");
-  catsSeparetedEl.appendChild(catMale);
+  $("#catsSep").append(catMale);
 
-  maleCats.forEach((element, index) => {
+  maleCats.forEach((element) => {
 
     //creo la classe cat
     let cat = document.createElement("div");
-    catMale.appendChild(cat);
-    //creo la classe cat color per l'assegnamento del colore tramite numerazione
+    catMale.append(cat);
+    //creo la classe cat color per l'assegnamento del colore
     let catColor = document.createElement("span");
-    catColor.classList.add(cont);
-    cat.appendChild(catColor);
+    cat.append(catColor);
     catColor.innerHTML += '<i class="fas fa-2x fa-cat"></i>';
     catColor.style.color = element.color;
     //creo la classe per il nome
     let catName = document.createElement("span");
-    cat.appendChild(catName);
+    cat.append(catName);
     catName.innerHTML += element.name;
     //creo la classe per l'assegnazione del sesso
     let catGender = document.createElement("span");
-    cat.appendChild(catGender);
+    cat.append(catGender);
     catGender.innerHTML += '<i class="fas fa-2x fa-mars"></i>';
     catGender.style.color = "blue";
     catGender.style.opacity = element.age;
-    cont++;
   });
   // combino i due array di oggetti in uno unico
   const catCombined = [...femaleCats, ...maleCats];
-
-  let catsCombEl = document.getElementById("catsComb");
 
   //ciclo l'array e inserisco nuovamente tutto sull'HTML
   catCombined.forEach((element) => {
@@ -138,20 +113,19 @@ $(document).ready(function() {
     //creo la classe cat
     let cat = document.createElement("div");
     cat.classList.add("col-lg-12");
-    catsCombEl.appendChild(cat);
-    //creo la classe cat color per l'assegnamento del colore tramite numerazione
+    $("#catsComb").append(cat);
+    //creo la classe cat color per l'assegnamento del colore
     let catColor = document.createElement("span");
-    catColor.classList.add(cont);
-    cat.appendChild(catColor);
+    cat.append(catColor);
     catColor.innerHTML += '<i class="fas fa-2x fa-cat"></i>';
     catColor.style.color = element.color;
     //creo la classe per il nome
     let catName = document.createElement("span");
-    cat.appendChild(catName);
+    cat.append(catName);
     catName.innerHTML += element.name;
     //creo la classe per l'assegnazione del sesso
     let catGender = document.createElement("span");
-    cat.appendChild(catGender);
+    cat.append(catGender);
     if(element.gender == "male"){
       catGender.innerHTML += '<i class="fas fa-2x fa-mars"></i>';
       catGender.style.color = "blue";
@@ -161,9 +135,5 @@ $(document).ready(function() {
       catGender.style.color = "pink";
       catGender.style.opacity = element.age;
     }
-    cont++;
   });
-
-
-
 });
